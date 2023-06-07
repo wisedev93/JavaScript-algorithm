2015cedf5b6622d7302635515eabb509812af630
jaehyunUm <wisedev93@gmail.com>
2023-06-07 23:24:04 +0900

    입출력 정리

--- input_output_test.js
+++ input_output_test.js
@@ -16,6 +16,7 @@
 // ------------- readLine -------------
 {
   const readline = require("readline");
+  // 비동기 임
   let rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
@@ -55,4 +56,76 @@
   const [e, f] = "1 2".split(" ").map((v) => +v); // === "1 2".split(" ").map(Number); === "1 2".split(" ").map((v) => Number(v));
   // Number(1), Number(2)의 return값을 바로 반환 한것과 같다
   console.log(e, f);
+}
+
+// ------------- input -------------
+// 1. 하나의 값을 입력받을 때
+{
+  const input = require("fs").readFileSync("./example.txt").toString().trim();
+  console.log(input);
+}
+// 2. 공백으로 구분된 한 줄의 값들을 입력받을 때
+{
+  const input = require("fs")
+    .readFileSync("./example.txt")
+    .toString()
+    .trim()
+    .split(" ");
+  console.log(input);
+}
+// 3. 여러 줄의 값들을 입력받을 때
+{
+  const input = require("fs")
+    .readFileSync("./example.txt")
+    .toString()
+    .trim()
+    .split("\n");
+  console.log(input);
+}
+// 4. 첫 번째 줄에 자연수 n을 입력받고, 그 다음줄에 공백으로 구분된 n개의 값들을 입력받을 때
+{
+  const [n, ...arr] = require("fs")
+    .readFileSync("./example.txt")
+    .toString()
+    .trim()
+    .split(/\s/);
+  console.log(n, arr);
+}
+// 5. 첫 번째 줄에 자연수 n을 입력받고, 그 다음줄부터 n개의 줄에 걸쳐 한 줄에 하나의 값을 입력받을 때
+{
+  const [n, ...arr] = require("fs")
+    .readFileSync("./example.txt")
+    .toString()
+    .trim()
+    .split("\n");
+  console.log(n, arr);
+}
+// 6. 하나의 값 또는 공백으로 구분된 여러 값들을 여러 줄에 걸쳐 뒤죽박죽 섞여서 입력받을 때
+// ex) n 입력 - 공백으로 구분된 n개의 값 입력 - m 입력 - 여러 줄에 걸쳐 m개의 값 입력
+{
+  const input = require("fs")
+    .readFileSync("./example.txt")
+    .toString()
+    .trim()
+    .split(/\s/); // === split(" ")
+  const n = input[0];
+  const n_arr = input.slice(1, n + 1);
+  const [m, ...m_arr] = input.slice(n + 1);
+  console.log(n, m, n_arr, m_arr);
+}
+// 2~6에서 입력받는 값들을 모두 String에서 Number로 바꾸려면 split()뒤에 .map(Number)를 추가
+
+// readFileSync의 return은 string이 아닌 buffer Object! toString()으로 정확하게 바꿔주자
+{
+  const input = require("fs").readFileSync("./example.txt");
+  console.log(typeof input); // object
+
+  const input2 = require("fs").readFileSync("./example.txt").toString();
+  console.log(typeof input2); // string
+
+  const input3 = require("fs").readFileSync("./example.txt") + "";
+  console.log(typeof input3); // string
+
+  const input4 = require("fs").readFileSync("./example.txt", "utf8"); // incoding해주면 string return
+  console.log(typeof input4); // string
 }
