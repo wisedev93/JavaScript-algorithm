5757091531bedc8a0a7e2b9e04a89f82c75ef2bb
jaehyunUm <wisedev93@gmail.com>
2023-07-11 23:48:04 +0900

    how to control flags?

--- programmers/lv0/코딩 기초 트레이닝/정수를 나선형으로 배치하기.js
+++ programmers/lv0/코딩 기초 트레이닝/정수를 나선형으로 배치하기.js
@@ -1,14 +1,35 @@
 function solution(n) {
-  //   const answer = new Array(n).fill(new Array(n).fill(0)); Whichi one is better?
-  const answer = Array.from({ length: n }).fill(
-    Array.from({ length: n }).fill(0)
-  );
-  const idx = [0, 0];
-
-  while (idx[0] * idx[1] <= 16) {
-    console.log(idx);
-    idx[0]++;
-    idx[1]++;
+  const answer = new Array(n);
+  // when using .fill(new Array(n)) method, why each items are reference copied?
+  for (let i = 0; i < n; i++) {
+    answer[i] = new Array(n);
+  }
+  let x = 0;
+  let y = 0;
+  let cnt = 1;
+  const flags = ["r", "d", "l", "u"];
+  let flag = flags[0];
+  while (cnt <= n ** 2) {
+    // if (y < n - 1) {
+    //   flag = flags[0];
+    // } else if (x < n - 1) {
+    //   flag = flags[1];
+    // } else if (x >= n - 1) {
+    //   flag = flags[2];
+    // }
+    // need flag controll
+    answer[x][y] = cnt;
+    console.log(flag);
+    if (flag === "r") {
+      y++;
+    } else if (flag === "d") {
+      x++;
+    } else if (flag === "l") {
+      y--;
+    } else if (flag === "u") {
+      x--;
+    }
+    cnt++;
   }
 
   return answer;
